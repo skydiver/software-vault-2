0f647163b91f49edabd715ec4ada404ff6e4a5db
Martín M <skydiver@users.noreply.github.com>
2019-12-16 02:42:45 -0300

    enable store on production

--- main/electron-store.js
+++ main/electron-store.js
@@ -2,13 +2,15 @@ const Store = require('electron-store');
 const electron = require('electron');
 const isDev = require('electron-is-dev');
 
+const pkg = require('../package.json');
+
 class ElectronStore {
   constructor() {
     const options = {};
-    // if (!isDev) {
-    //   options.name = 'serials-database';
-    //   options.cwd = electron.remote.app.getPath('documents');
-    // }
+    if (!isDev) {
+      options.name = pkg.name;
+      options.cwd = electron.remote.app.getPath('documents');
+    }
     this.STORE = new Store(options);
   }
 
